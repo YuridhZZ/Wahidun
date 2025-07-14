ba4dce269b2a047f9925acdb37ef44ae48fe12dc
YuridhZZ <yuridhzahrawan@gmail.com>
2025-07-14 16:10:22 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <footer className="bg-white rounded-lg shadow m-4">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                 <span className="text-sm text-gray-500 sm:text-center">
-                    © 2023 FlowBank™. All Rights Reserved.
+                    © 2025 WahidunBank™. All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
                     <li>
